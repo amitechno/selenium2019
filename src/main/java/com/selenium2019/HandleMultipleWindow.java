e2d98668d66318fdc5ebd59d2f6dd14d4f80ceed
Amit Sharma <amitsharma@Amits-MacBook-Pro.local>
2018-12-24 19:38:37 +0530

    added wait method

--- src/main/java/com/selenium2019/HandleMultipleWindow.java
+++ src/main/java/com/selenium2019/HandleMultipleWindow.java
@@ -6,6 +6,8 @@ import java.util.Set;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class HandleMultipleWindow {
@@ -22,6 +24,8 @@ public class HandleMultipleWindow {
 		System.out.println(globalPath);
 		driver = new ChromeDriver();
 		driver.get("http://automationpractice.com/index.php");
+		new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='social_block']/ul/li[3]/a")));
+		
 		driver.findElement(By.xpath("//*[@id='social_block']/ul/li[3]/a")).click();
 		Set<String> windowId = driver.getWindowHandles();
 		Iterator<String> itr = windowId.iterator();
@@ -29,13 +33,15 @@ public class HandleMultipleWindow {
 		String childId = itr.next();
 		driver.switchTo().window(childId);
 		try {
-			Thread.sleep(6000);
+			Thread.sleep(10000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		driver.close();
 		driver.switchTo().window(parentId);
+		new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='footer']/div/section[2]/div/div/ul/li/a")));
+		
 		driver.findElement(By.xpath("//*[@id='footer']/div/section[2]/div/div/ul/li/a")).click();
 		driver.quit();
 		
